7424bd57e3afe055c5a0472c723a7252c7494845
Andrey Chorniy <creatuseandr@icloud.com>
2022-05-20 15:49:37 +0300

    add files for - sorting table and convert to excel for admins

---
+++ public/js/orderTable.js
@@ -0,0 +1,11 @@
+// connecting table sorting
+new Tablesort(document.querySelector(".table"));
+
+// downloading table to excel file for administrators
+
+document
+  .querySelector(".btn-download")
+  .addEventListener("click", function name(params) {
+    let tableToExcel = new Table2Excel();
+    tableToExcel.export(document.querySelector(".table"));
+  });
