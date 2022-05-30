f91647e901dcea76c2fa006e59ed0259eafb40a9
Andrey Chorniy <creatuseandr@icloud.com>
2022-05-30 16:38:07 +0300

    change slider settings

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,9 @@
 let slider = tns({
   container: ".my-slider",
-  items: 3,
+  items: 2,
   autoplay: true,
   speed: 900,
   controls: false,
   nav: false,
-  // gutter: 5,
-  responsive: {
-    "1430": {
-      items: 3,
-    },
-    "860": {
-      items: 2,
-    },
-    "350": {
-      items: 1,
-    },
-  },
+  // center: true,
 });
