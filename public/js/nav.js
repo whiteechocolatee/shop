9dd9ec35b473194ff5961261cad50ea5368693f2
Andrey Chorniy <creatuseandr@icloud.com>
2022-05-30 16:38:11 +0300

    Update nav.js

--- public/js/nav.js
+++ public/js/nav.js
@@ -2,7 +2,7 @@ let navbar = document.getElementById("navbar").classList;
 let active_class = "navbar_scrolled";
 
 window.addEventListener("scroll", (e) => {
-  if (pageYOffset > 200) {
+  if (pageYOffset > 300) {
     navbar.add(active_class);
   } else {
     navbar.remove(active_class);
