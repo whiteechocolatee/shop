672f51b52491fe11ed6f9c45d137dce97dfb94fd
Andrey Chorniy <creatuseandr@icloud.com>
2022-05-31 18:57:16 +0300

    changed showing navbar while scrolled

--- public/js/nav.js
+++ public/js/nav.js
@@ -2,7 +2,7 @@ let navbar = document.getElementById("navbar").classList;
 let active_class = "navbar_scrolled";
 
 window.addEventListener("scroll", (e) => {
-  if (pageYOffset > 300) {
+  if (pageYOffset > 100) {
     navbar.add(active_class);
   } else {
     navbar.remove(active_class);
