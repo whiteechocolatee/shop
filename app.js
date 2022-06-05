e7d3cdb83df25e4479a7af55ccdcfb8bec451fb0
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-05 19:49:10 +0300

    edited server and added some info

--- app.js
+++ app.js
@@ -42,15 +42,10 @@ const con = mysql.createConnection({
 const upload = multer({ storage: storage });
 
 app
-  // reading json
   .use(express.json())
-  // adding middleware function for parsing requset body
   .use(express.urlencoded())
-  // adding cookie parser
   .use(cookieParser())
-  // static files at server
   .use(express.static(__dirname + "/public"))
-  // adding hash validation to admin panel
   .use((req, res, next) => {
     if (req.originalUrl == "/admin" || req.originalUrl == "/admin-orders") {
       hashValidation(res, req, con, next);
@@ -65,97 +60,83 @@ app.listen(port, hostname, () => {
   console.log(`Server working on http://${hostname}:${port}/main`);
 });
 
-//
-// rendering pages with static files
-//
-
-// render order page
-app.get("/main/order", function (req, res) {
-  res.render("orderPage");
-});
-// admin panel
-app.get("/admin", function (req, res) {
-  res.render("adminPanel", {});
-});
-// create login to admin panel
-app.get("/login", function (req, res) {
-  res.render("loginPage", {});
-});
-// agreement url for terms and coditions
-app.get("/main/order/agreement", function (req, res) {
-  res.render("agreement", {});
-});
-//adding new item
-app.get("/admin/goods/createNewItem", function (req, res) {
-  res.render("addNewItem");
-});
-
-//
-// sending data , and rendering pages with data
-//
-
-// getting main page
-app.get("/main", function (req, res) {
-  lastUploadedFiles(req, res, "main");
-});
-// get adress of categories with id
-app.get("/main/category", function (req, res) {
-  pageOfCategories(req, res, "category");
-});
-//sending items to goods page
-app.get("/main/category/goods", function (req, res) {
-  gettingProductInformation(req, res, "goods");
-});
-// adminPanel orders
-app.get("/admin/orders", function (req, res) {
-  orderTablePage(req, res, "orderPanel");
-});
-// rendering admin goods
-app.get("/admin/goods", function (req, res) {
-  goodsData(req, res, "adminGoods");
-});
-//edit card page
-app.get("/admin/goods/edit", function (req, res) {
-  gettingProductInformation(req, res, "editPage");
-});
-// update data goods
-app.post("/updateItem", function (req, res) {
-  updatingItems(req, res);
-});
-// saving new goods to data base
-app.post("/addingNewItem", function (req, res) {
-  addingItems(req, res);
-});
-// posting login data
-app.post("/login", function (req, res) {
-  updatingLoginHash(req, res);
-});
-// getting categories to navigation
-app.get("/gettingCategories", function (req, res) {
-  gettingCategories(req, res);
-});
-// showing items in cart
-app.post("/cartGoodsInfo", function (req, res) {
-  showingCart(req, res);
-});
-// posting data at database
-app.post("/endOfTheOrder", function (req, res) {
-  savingCartData(req, res);
-});
-
-//
-// saving img by multer and redirecting to goods page at admin panel
-//
-
-// saving image to folder
-app.post("/addingNewImage", upload.single("image"), function (req, res) {
-  res.redirect("/admin/goods");
-});
-
-// saving edited image to folder
-app.post("/uploadEditedImage", upload.single("editedImage"), function (
-  req,
-  res
-) {
-  res.redirect("/admin/goods");
-});
+app
+  // rendering form page
+  .get("/main/order", function (req, res) {
+    res.render("orderPage");
+  })
+  // rendering agreement for customer
+  .get("/main/order/agreement", function (req, res) {
+    res.render("agreement");
+  })
+  // login form for admins
+  .get("/login", function (req, res) {
+    res.render("loginPage");
+  })
+  // rendering admin panel
+  .get("/admin", function (req, res) {
+    res.render("adminPanel");
+  })
+  // rendering page where admin can create new item
+  .get("/admin/goods/createNewItem", function (req, res) {
+    res.render("addNewItem");
+  })
+  // rendering lat uploaded files
+  .get("/main", function (req, res) {
+    lastUploadedFiles(req, res, "main");
+  })
+  // rendering categories page
+  .get("/main/category", function (req, res) {
+    pageOfCategories(req, res, "category");
+  })
+  // rendering one item page
+  .get("/main/category/goods", function (req, res) {
+    gettingProductInformation(req, res, "goods");
+  })
+  // rendering admin order table
+  .get("/admin/orders", function (req, res) {
+    orderTablePage(req, res, "orderPanel");
+  })
+  // rendering all categories at navigation panel
+  .get("/gettingCategories", function (req, res) {
+    gettingCategories(req, res);
+  })
+  // rendering editing page at admin panel
+  .get("/admin/goods/edit", function (req, res) {
+    gettingProductInformation(req, res, "editPage");
+  })
+  // rendering admin goods gallery
+  .get("/admin/goods", function (req, res) {
+    goodsData(req, res, "adminGoods");
+  })
+  // sending request to create new item
+  .post("/addingNewItem", function (req, res) {
+    addingItems(req, res);
+  })
+  // showing cart at order page
+  .post("/cartGoodsInfo", function (req, res) {
+    showingCart(req, res);
+  })
+  // create new user and order at database
+  .post("/endOfTheOrder", function (req, res) {
+    savingCartData(req, res);
+  })
+  // uploading hash when new admin log in
+  .post("/login", function (req, res) {
+    updatingLoginHash(req, res);
+  })
+  // sending request to updating existing item
+  .post("/updateItem", function (req, res) {
+    updatingItems(req, res);
+  })
+  // saving new images at folder
+  .post("/addingNewImage", upload.single("image"), function (req, res) {
+    res.redirect("/admin/goods");
+  })
+  // uploading edited image by admin
+  .post("/uploadEditedImage", upload.single("editedImage"), function (
+    req,
+    res
+  ) {
+    res.redirect("/admin/goods");
+  });
