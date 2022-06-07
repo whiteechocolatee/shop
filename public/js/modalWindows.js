ce6d4dc1302191e06b5dfeb08c8580834ae99432
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-07 18:09:24 +0300

    added new modal window and importing to order page

---
+++ public/js/modalWindows.js
@@ -0,0 +1,13 @@
+const Toast = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 4000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener("mouseenter", Swal.stopTimer);
+    toast.addEventListener("mouseleave", Swal.resumeTimer);
+  },
+});
+
+export default Toast;
