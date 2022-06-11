4192ab45848cbcbe17e015e2014561a48481665b
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-11 17:17:29 +0300

    edit code

--- server/gettingMethods/lastUploadedFiles.js
+++ server/gettingMethods/lastUploadedFiles.js
@@ -8,15 +8,15 @@ const con = mysql.createConnection({
 });
 
 module.exports = function lastUploadedFiles(req, res, renderingPage) {
-  // taking 3 last added goods and rendering main page
+  // taking 3 goods from every category and rendering main page
   let goodsName = new Promise(function (resolve, reject) {
-    con.query(
-      `SELECT * FROM goods ORDER BY id DESC LIMIT 6`,
-      function (err, result) {
-        if (err) reject(err);
-        resolve(result);
-      }
-    );
+    con.query(`SELECT * FROM goods WHERE category LIMIT 6`, function (
+      err,
+      result
+    ) {
+      if (err) reject(err);
+      resolve(result);
+    });
   });
 
   // taking all categories for rendering main page
--- server/gettingMethods/pageOfCategories.js
+++ server/gettingMethods/pageOfCategories.js
@@ -7,9 +7,12 @@ const con = mysql.createConnection({
   database: "market",
 });
 
+const resultsPerPage = 6;
+
 module.exports = function pageOfCategories(req, res, renderingPage) {
   let categoryId = req.query["id"];
-  // get category id,and getting data from db
+
+  // getting category
   let category = new Promise((resolve, reject) => {
     con.query("SELECT * FROM category WHERE id=" + categoryId, function (
       err,
@@ -19,17 +22,40 @@ module.exports = function pageOfCategories(req, res, renderingPage) {
       resolve(result);
     });
   });
-  // get goods from db
+
+  // getting goods
   let goods = new Promise((resolve, reject) => {
-    con.query("SELECT * FROM goods WHERE category=" + categoryId, function (
+    con.query(`SELECT * FROM goods WHERE category=` + categoryId, function (
       err,
       result
     ) {
       if (err) reject(err);
-      resolve(result);
+      const numOfResults = result.length;
+      const numberOfPages = Math.ceil(numOfResults / resultsPerPage);
+      let page = req.query.page ? Number(req.query.page) : 1;
+      if (page > numberOfPages) {
+        res.redirect(`&page=` + encodeURIComponent(numberOfPages));
+      } else if (page < 1) {
+        res.redirect("&page=" + encodeURIComponent("1"));
+      }
+      const startingLimit = (page - 1) * resultsPerPage;
+      sql = `SELECT * FROM goods WHERE category=${categoryId} LIMIT ${startingLimit},${resultsPerPage} `;
+      con.query(sql, (err, result) => {
+        if (err) throw err;
+        let iterator = page - 5 < 1 ? 1 : page - 5;
+        let endingLink =
+          iterator + 9 <= numberOfPages
+            ? iterator + 9
+            : page + (numberOfPages - page);
+        if (endingLink < page + 4) {
+          iterator -= page + 4 - numberOfPages;
+        }
+        resolve(result);
+      });
     });
   });
 
+  // getting types
   let types = new Promise((resolve, reject) => {
     con.query(
       "SELECT DISTINCT type, data_type FROM goods WHERE category=" + categoryId,
